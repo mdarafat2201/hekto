9c0b85ba3dfdc7bcde7a0253f5c6e2c01870c264
mdarafat2201 <maccenarafat@gmail.com>
2024-04-14 12:23:21 +0100

    LeatestProducts overle don

--- src/Component/CommonComponent/newproducts.jsx
+++ src/Component/CommonComponent/newproducts.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import productsImg5 from "../../assets/productsImg5.png";
 import Flex from "../CommonComponent/Flex";
+import { GiSelfLove } from "react-icons/gi";
+import { FiShoppingCart } from "react-icons/fi";
+import { FaSearchPlus } from "react-icons/fa";
 const newproducts = ({ img, title, dichcont, prich }) => {
   return (
     <>
-      <div className="w-[360px] py-[58px]">
-        <div className="bg-secondary_bg_color h-[270px] pl-[50px] pt-8 pb-2">
+      <div className="w-[360px] py-[58px] relative group overflow-hidden ">
+        <div className="bg-secondary_bg_color hover:bg-main_bg_color h-[270px] pl-[50px] pt-8 pb-2">
           <img src={img ? img : productsImg5} alt={productsImg5} />
         </div>
         <Flex className={"items-center justify-between mt-4"}>
@@ -29,6 +32,28 @@ const newproducts = ({ img, title, dichcont, prich }) => {
             </div>
           </Flex>
         </Flex>
+
+        {/* Overlae */}
+        <div className="top-[70px] -left-20  absolute group-hover:left-2 transition-all">
+          <div className="bg-green-300 rounded mb-[70px]">
+            <h3 className=" font-JosefinSans text-sm font-normal text-center text-main_bg_color">
+              Sale
+            </h3>
+          </div>
+          <div>
+            <Flex className={"items-center flex-col"}>
+              <div className="text-main_font_color w-[46px] p-[15px] rounded-full hover:bg-green-200 hover:text-[#08D15F]">
+                <FiShoppingCart className="cursor-pointer" />
+              </div>
+              <div className="text-main_font_color w-[46px] p-[15px] rounded-full hover:bg-green-200 hover:text-[#08D15F]">
+                <GiSelfLove className="cursor-pointer" />
+              </div>
+              <div className="text-main_font_color w-[46px] p-[15px] rounded-full hover:bg-green-200 hover:text-[#08D15F]">
+                <FaSearchPlus className="cursor-pointer" />
+              </div>
+            </Flex>
+          </div>
+        </div>
       </div>
     </>
   );
