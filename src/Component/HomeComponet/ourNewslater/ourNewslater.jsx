0ff3e973c5005544f88539617e202b3794b959b6
mdarafat2201 <maccenarafat@gmail.com>
2024-04-19 12:43:19 +0100

    Our Newslater r don

--- src/Component/HomeComponet/ourNewslater/ourNewslater.jsx
+++ src/Component/HomeComponet/ourNewslater/ourNewslater.jsx
@@ -7,18 +7,23 @@ const ourNewslater = () => {
     <>
       <div className="relative">
         <img src={bannerimg2} alt={bannerimg2} />
-        <div className="left-[420px] top-[100px]  absolute">
-          <h2 className="text-center mb-8 font-JosefinSans  text-4xl text-main_font_color font-bold">
+
+        <div className="lg:left-[420px] md:left-20 left-14  lg:top-[100px] md:top-5  top-0 absolute">
+          <h2 className="text-center lg:mb-8 md:mb-6 sm:mb-4 mb-1 font-JosefinSans text-sm  sm:text-4xl text-main_font_color font-bold">
             Get Leatest Update By Subscribe <br /> Our Newslater
           </h2>
-          <Button
-            className={"py-3 px-8 ml-[212px]  bg-secondary_font_color"}
-            title={"Shop Now"}
-          />
+          <div className="mx-14 sm:mx-0">
+            <Button
+              className={
+                "sm:py-3 py-2 sm:px-8 px-6 sm:mx-[212px]   bg-secondary_font_color"
+              }
+              title={"Shop Now"}
+            />
+          </div>
         </div>
       </div>
       <div className="container">
-        <div className="my-20 mx-[250px]">
+        <div className="lg:my-20 py-8 mx-5 lg:mx-[250px]">
           <img src={logoItem} alt={logoItem} />
         </div>
       </div>
